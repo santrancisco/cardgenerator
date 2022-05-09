90e2f7f0ca62a3ca9dbe37e5a882eeb94c1d2d1f
santrancisco <san.tran@ebfe.pw>
2022-05-09 12:13:10 +1000

    first commit

---
+++ defaultcards.js
@@ -0,0 +1,226 @@
+data = [
+  {
+    "img": "https://cdn4.vectorstock.com/i/1000x1000/77/18/businessman-looking-through-binoculars-lead-vector-7547718.jpg",
+    "heading": "Enumeration",
+    "text": "The process of extracting user names, network resources and more from a system. The attacker creates an active connection to the system and performs queries to gain more information about the target.",
+    "color": "#FF076E",
+    "type": "attack"
+  },
+  {
+    "img": "https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcQagHvoQm1CQpxuJTUcdthJFHyUoNJuFzMYOoY9KywxhGR1CVj2",
+    "heading": "Credential Stuffing",
+    "text": "Credential stuffing is the automated injection of breached username/password pairs in order to fraudulently gain access to user accounts.",
+    "color": "#FF076E",
+    "type": "attack"
+  },
+  {
+    "img": "https://innovationorigins.com/wp-content/uploads/2018/10/traffic-jam-file-new-delhi-1024x640.jpg",
+    "heading": "Denial of Service",
+    "text": "DoS attacks typically function by overwhelming a targeted machine with requests until normal traffic is unable to be processed, resulting in denial-of-service to users. ",
+    "color": "#FF076E",
+    "type": "attack"
+  },
+  {
+    "img": "https://blog.malwarebytes.com/wp-content/uploads/2018/09/shutterstock_749866270-900x506.jpg",
+    "heading": "Phishing",
+    "text": "The fraudulent practice of sending emails purporting to be from reputable companies in order to induce individuals to reveal personal information.",
+    "color": "#FF076E",
+    "type": "attack"
+  },
+  {
+    "img": "https://www.ptsecurity.com/upload/corporate/ww-en/images/news/sql-injection-attacks.jpg",
+    "heading": "SQL Injection",
+    "text": "The placement of malicious code in SQL statements, via web page input.",
+    "color": "#FF076E",
+    "type": "attack"
+  },
+  {
+    "img": "https://vignette.wikia.nocookie.net/scoobydoo/images/b/bc/Grandma_%282002_film%29.png/revision/latest?cb=20170829121546",
+    "heading": "Spoofing",
+    "text": "The act of disguising a communication from an unknown source as being from a known, trusted source.",
+    "color": "#FF076E",
+    "type": "attack"
+  },
+  {
+    "img": "https://media.npr.org/assets/img/2017/10/12/trh_manipulation_artwork1_wide-b3b9648e2f6fb9a829cc1d800adf7dada2e5f57e-s800-c85.jpg",
+    "heading": "Social Engineering",
+    "text": "The art of manipulating people so they give up confidential information.",
+    "color": "#FF076E",
+    "type": "attack"
+  },
+  {
+    "img": "https://janehinrichs.weebly.com/uploads/5/0/6/2/5062227/437864_orig.jpg",
+    "heading": "Cross-Site Scripting",
+    "text": "XSS attacks occur when an attacker uses a web application to send malicious code, generally in the form of a browser side script, to a different end user. The end user’s browser has no way to know that the script should not be trusted, and will execute the script.",
+    "color": "#FF076E",
+    "type": "attack"
+  },
+  {
+    "img": "https://vignette.wikia.nocookie.net/starwars/images/4/44/End_Days.jpg/revision/latest?cb=20111028234105",
+    "heading": "Remote Code Execution",
+    "text": "A vulnerability where an attacker is able to run code of their choosing with system level privileges on a server that possesses the appropriate weakness.",
+    "color": "#FF076E",
+    "type": "attack"
+  },
+  {
+    "img": "https://sebastiancorp.com/wp-content/uploads/2014/08/ID-10061672.jpg",
+    "heading": "Network Intrusion",
+    "text": "Any unauthorized activity on a digital network, often involving the theft of valuable network resources.",
+    "color": "#FF076E",
+    "type": "attack"
+  },
+  {
+    "img": "https://www.lifewire.com/thmb/P856-0hi4lmA2xinYWyaEpRIckw=/1920x1326/filters:no_upscale():max_bytes(150000):strip_icc()/cloud-upload-a30f385a928e44e199a62210d578375a.jpg",
+    "heading": "Unrestricted File Upload",
+    "text": "The first step in many attacks is to get some code to the target system. Then the attacker only needs to find a way to get the code executed. Using a file upload helps the attacker accomplish this step. Consequences including complete system takeover, an overloaded database, and more.",
+    "color": "#FF076E",
+    "type": "attack"
+  },
+  {
+    "img": "https://cdn.vox-cdn.com/thumbor/adFYi1L1RT0rLVSX9EJIKt1CX_E=/0x0:2040x1360/1200x675/filters:focal(872x194:1198x520)/cdn.vox-cdn.com/uploads/chorus_image/image/65852159/IMG_7DA316599F72_1.0.jpeg",
+    "heading": "Insecure Direct Object Reference (IDOR)",
+    "text": "A security vulnerability in which a user is able to access and make changes to data of any other user present in the system.",
+    "color": "#FF076E",
+    "type": "attack"
+  },
+  {
+    "img": "https://image.shutterstock.com/image-photo/male-security-guard-sleeping-workplace-260nw-1008333160.jpg",
+    "heading": "Broken Authorization",
+    "text": "In poorly designed authorization systems, access control rules are inserted in various locations all over the code. Many of these flawed access control schemes are not difficult for attackers to discover and exploit.",
+    "color": "#FF076E",
+    "type": "attack"
+  },
+  {
+    "img": "https://specials-images.forbesimg.com/imageserve/5e2ab3808b6cf300071cd59e/960x0.jpg?fit=scale",
+    "heading": "Malware",
+    "text": "Malware, or “malicious software,” is an umbrella term that describes any malicious program or code that is harmful to systems.",
+    "color": "#FF076E",
+    "type": "attack"
+  },
+  {
+    "img": "https://goombastomp.com/wp-content/uploads/2020/01/Malcolm-in-the-Middle-Review.jpg",
+    "heading": "Man-in-the-Middle",
+    "text": "A man in the middle (MITM) attack is a general term for when a perpetrator positions himself in a conversation between a user and an application - either to eavesdrop or to impersonate one of the parties, making it appear as if a normal exchange of information is underway.",
+    "color": "#FF076E",
+    "type": "attack"
+  },
+  {
+    "img": "https://static.independent.co.uk/s3fs-public/thumbnails/image/2016/12/24/09/nuclear-bomb.jpg?w968h681",
+    "heading": "Logic Bomb",
+    "text": "A piece of code that will set off a malicious function when specified conditions are met.",
+    "color": "#FF076E",
+    "type": "attack"
+  },
+  {
+    "img": "https://www.racksolutions.com/news//app/uploads/AdobeStock_173122779.jpeg",
+    "heading": "Server Side Request Forgery (SSRF)",
+    "text": "An exploit where the attacker can abuse functionality on the server to read or update internal resources.",
+    "color": "#FF076E",
+    "type": "attack"
+  },
+  {
+    "img": "https://www.ibm.com/blogs/blockchain/wp-content/uploads/2019/11/Retina_Display-1097174122.jpg",
+    "heading": "Supply Chain Attack",
+    "text": "A supply chain attack occurs when someone infiltrates your system through an outside partner or provider with access to your systems and data.",
+    "color": "#FF076E",
+    "type": "attack"
+  },
+  {
+    "img": "https://www.geosurf.com/wp-content/uploads/2019/04/firewall.jpg",
+    "heading": "Web Application Firewall",
+    "text": "An application firewall for HTTP applications. It applies a set of rules to an HTTP conversation, generally covering common attacks such as Cross-site Scripting (XSS) and SQL Injection.",
+    "color": "#0b30ca",
+    "type": "defence"
+  },
+  {
+    "img": "https://cdn3.whatculture.com/images/2014/09/Blade-Runner-Test.jpg",
+    "heading": "Anti-Automation",
+    "text": "A service that detects non-human traffic. It can issue challenges (such as a reCaptcha) to confirm that the traffic is human, blocking non-human requests.",
+    "color": "#0b30ca",
+    "type": "defence"
+  },
+  {
+    "img": "https://rivernetcomputers.com/wp-content/uploads/2018/10/1200x630wa.png",
+    "heading": "Two Factor Authentication",
+    "text": "A way of 'double-checking' that you’re really the person you’re claiming to be when you log into your online accounts, usually consisting of a password and code sent to your mobile device.",
+    "color": "#0b30ca",
+    "type": "defence"
+  },
+  {
+    "img": "https://cdn.vox-cdn.com/thumbor/ZrPgeOg-Lmhd4MmWpTOIKB8FH4o=/123x0:1469x897/1200x800/filters:focal(123x0:1469x897)/cdn.vox-cdn.com/uploads/chorus_image/image/46104024/hulkbuster.0.0.jpg",
+    "heading": "Application Hardening",
+    "text": "The process of taking a finished application and making it more difficult to reverse engineer and tamper. Involves manipulating existing and injecting new code to shield the application against static and dynamic attacks.",
+    "color": "#0b30ca",
+    "type": "defence"
+  },
+  {
+    "img": "https://www.compuquip.com/hs-fs/hubfs/images/blog-images/firewall-monitoring-best-practices.jpg?width=600&name=firewall-monitoring-best-practices.jpg",
+    "heading": "Network Firewall",
+    "text": "A security device used to stop or mitigate unauthorized access to private networks connected to the Internet, especially intranets. The only traffic allowed on the network is defined via firewall policies.",
+    "color": "#0b30ca",
+    "type": "defence"
+  },
+  {
+    "img": "https://i.pinimg.com/originals/c3/1f/4e/c31f4eaf5c1f3f0de2586b1c65b6e4de.png",
+    "heading": "Malware Scanner",
+    "text": "Software that performs a deep scan of the computer to prevent malware infection. ",
+    "color": "#0b30ca",
+    "type": "defence"
+  },
+  {
+    "img": "https://resource.chemlinked.com.cn/food/articles/8g7uiN8btmssfgex5aqq.jpg",
+    "heading": "Audit Software",
+    "text": "Specialized programs that perform a variety of audit functions, such as sampling databases and generating confirmation letters to customers and vendors. It can highlight exceptions to categories of data and alert the examiner to possible error.",
+    "color": "#0b30ca",
+    "type": "defence"
+  },
+  {
+    "img": "https://i.pinimg.com/originals/04/00/1d/04001de702419d62f2649133e1c43354.jpg",
+    "heading": "Application Whitelisting",
+    "text": "An approach that allows only pre-approved and specified programs to run. Any program not whitelisted is blocked by default.",
+    "color": "#0b30ca",
+    "type": "defence"
+  },
+  {
+    "img": "https://aphyr.com/data/photographs/14/medium.jpg",
+    "heading": "Indirect Reference Token",
+    "text": "A security measure that prevents attackers from directly targeting unauthorized resources.",
+    "color": "#0b30ca",
+    "type": "defence"
+  },
+  {
+    "img": "https://www.rockwellautomation.com.cn/resources/images/rockwellautomation/publication/shutterstock_626817371--photograph_848w477h.jpg",
+    "heading": "Monitoring and Alerts",
+    "text": "An incredibly useful component in active security investigations, accelerating event discovery and resolution processes.",
+    "color": "#0b30ca",
+    "type": "defence"
+  },
+  {
+    "img": "https://media.kasperskydaily.com/wp-content/uploads/sites/92/2018/05/15093145/secure-element-featured.jpg",
+    "heading": "Secure Communication",
+    "text": "Measures and controls taken to deny unauthorized persons information transferred over a network.",
+    "color": "#0b30ca",
+    "type": "defence"
+  },
+  {
+    "img": "https://www.colocationamerica.com/images/network-segmentation.png",
+    "heading": "Network Segmentation",
+    "text": "Segmentation works by controlling how traffic flows among the parts of a network application. You could choose to stop all traffic in one part from reaching another, or you can limit the flow by traffic type, source, destination, and many other options.",
+    "color": "#0b30ca",
+    "type": "defence"
+  },
+  {
+    "img": "https://d2slcw3kip6qmk.cloudfront.net/marketing/techblog/json-bad-for-cfg-post-header@2x.png",
+    "heading": "Configuration Management",
+    "text": "A systems engineering process for establishing and maintaining consistency of a product's performance, functional, and physical attributes with its requirements, design, and operational information throughout its life.",
+    "color": "#0b30ca",
+    "type": "defence"
+  },
+  {
+    "img": "https://i.pinimg.com/originals/91/ee/23/91ee23af415a35e0bbddea2e0d81cc16.jpg",
+    "heading": "User Access Policy",
+    "text": "Outlines the controls placed on both physical access to the computer system and to the software in order to limit access to computer networks and data.",
+    "color": "#0b30ca",
+    "type": "defence"
+  }
+];  
